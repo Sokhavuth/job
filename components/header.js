0011a0e88f0f1ef8006f35c0bbaf69b7760b6cb5
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-01 16:08:39 +0700

    Make it better

---
+++ components/header.js
@@ -0,0 +1,36 @@
+import {useState, useEffect} from 'react'
+import style from '../styles/Header.module.scss'
+import Link from 'next/link'
+import $ from 'jquery'
+
+export default function Header(){
+  let menuDropdown = false
+
+  const openMobileMenu = () => {
+    var x = document.getElementById('topmenu')
+    if(menuDropdown){
+      x.className = style.topnav
+      menuDropdown = false
+    }else{
+      x.className += ' ' + style.responsive
+      menuDropdown = true
+    }
+  }
+
+  return(
+    <div className={style.Header}>
+      <div className={`${style.menubar} region`}>
+        <div className={style.logo}><Link href='/'><a>Khwebjob</a></Link></div> 
+        <div className={`${style.topnav} topnav`} id="topmenu">
+          <Link href=''><a href="#home" className={style.active}>Home</a></Link>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+          <a href="javascript:void(0);" className={style.icon} onClick={openMobileMenu}>
+            <i class="fa fa-bars"></i>
+          </a>
+        </div>
+        <div className={style.login} ><Link href='/login'><a>Login</a></Link></div>
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from '../components/header'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Jon Announcement</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Khmer Web Job</title>
+        <link rel="icon" href="/images/site_logo.png" />
+        <link rel='stylesheet' href='/fonts/setup.css' />
+        <link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        </link>
       </Head>
 
-      <main className={styles.main}>
-        Welcome to Job Announcement Website!
-      </main>
+      <header>
+        <Header />
+      </header>
+
+      <div className={`${styles.panel}`}>
+        <img alt='' src='/images/background.jpg' />
+      </div>
 
       <footer className={styles.footer}>
         <a
