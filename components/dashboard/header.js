db4e34466e43869724a5a8ebdeb792ee27286d60
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-03 14:59:45 +0700

    Make it better

--- components/dashboard/header.js
+++ components/dashboard/header.js
@@ -1,40 +1,55 @@
 import VHead from '../head'
 import style from '../../styles/dashboard/Header.module.scss'
 import Link from 'next/link'
-import { getFetch } from '../../tool'
 import Router from 'next/router'
+import { useState, useEffect } from 'react'
+import { getFetch, postFetch } from '../../tool'
 
 export default function Header() {
 
+  let [header, setHeader] = useState('')
+
+  useEffect( async () => {
+    const logged = await getFetch('/api/users/logged')
+    if (logged.user) {
+      setHeader(
+        header = (
+          <header className={style.header}>
+            <div className={`${style.menubar} region`}>
+              <div className={style.logo}>
+                <Link href='/dashboard'><a> Dashboard </a></Link>
+              </div>
+              <form className={style.search}>
+                <select name='options'>
+                  <option>Post</option>
+                  <option>Page</option>
+                  <option>Category</option>
+                  <option>User</option>
+                </select>
+                <input type='search' name='query' />
+                <input type='submit' value='Submit' />
+              </form>
+              <div className={style.logout}>
+                <a onClick={logout}>Logout</a>
+              </div>
+            </div>
+          </header>
+        )
+      )
+    } else {
+      Router.push('/login')
+    }
+  })
+
   const logout = async () => {
-    await fetch('/api/users/logout', { method: "POST" })
+    await postFetch('/api/users/logout', { method: "POST" })
     Router.push('/')
   }
 
   return(
     <div className={style.Header}>
       <VHead />
-
-      <header className={style.header}>
-        <div className={`${style.menubar} region`}>
-          <div className={style.logo}>
-            <Link href='/dashboard'><a> Dashboard </a></Link>
-          </div>
-          <form className={style.search}>
-              <select name='options'>
-                <option>Post</option>
-                <option>Page</option>
-                <option>Category</option>
-                <option>User</option>
-              </select>
-              <input type='search' name='query' />
-              <input type='submit' value='Submit' />
-            </form>
-            <div className={style.logout}>
-              <a onClick={logout}>Logout</a>
-            </div>
-        </div>
-      </header>
+      {header}
     </div>
   )
 }
