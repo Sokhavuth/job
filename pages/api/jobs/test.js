c905631bd889b74d6bb661a25ecc210f1de1a15e
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-10 08:21:58 +0700

    Make it better

---
+++ pages/api/jobs/test.js
@@ -0,0 +1,15 @@
+import schema from './schema'
+
+export default async function getJob (req, res) {
+  const jobSchema = await schema()
+  //const job = await jobSchema.find({ id: req.body.id })
+  res.json({ job: job })
+}
+
+async function get_Job (id) {
+  const jobSchema = await schema()
+  const job = await jobSchema.findOne({ id: id })
+  return { job: JSON.stringify(job) }
+}
+
+module.exports = get_Job
