026d336da73c086322107eb4cba488d96d0551f8
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-13 19:50:41 +0700

    Make it better

--- pages/api/jobs/initiate.js
+++ pages/api/jobs/initiate.js
@@ -3,14 +3,13 @@ import schema from './schema'
 async function getJobs (amount, category=null) {
   const jobSchema = await schema()
   if (category) {
-    var jobs = await jobSchema.find({ categories: category }).sort({postdate: -1, _id: -1}).limit(amount)
+    var jobs = await jobSchema.find({ enddate: {$gte: new Date()}, categories: category }).sort({postdate: -1, _id: -1}).limit(amount)
     return  JSON.stringify(jobs)
   } else {
-    var jobs = await jobSchema.find().sort({postdate: -1, _id: -1}).limit(amount)
+    var jobs = await jobSchema.find({ enddate: {$gte: new Date()} }).sort({postdate: -1, _id: -1}).limit(amount)
     var count = await jobSchema.countDocuments({})
     return  JSON.stringify({ jobs: jobs, count: count })
   }
-
   
 }
 
--- pages/api/jobs/navigate.js
+++ pages/api/jobs/navigate.js
@@ -6,9 +6,9 @@ export default async function getJob (req, res) {
   const amount = setting.frontPagePostLimit
   
   if (req.body.name){
-    var jobs = await _schema.find({ categories: req.body.name }).skip(amount * req.body.page).sort({postdate: -1, _id: -1}).limit(amount)
+    var jobs = await _schema.find({ enddate: {$gte: new Date()}, categories: req.body.name }).skip(amount * req.body.page).sort({postdate: -1, _id: -1}).limit(amount)
   }else{
-    var jobs = await _schema.find().skip(amount * req.body.page).sort({postdate: -1, _id: -1}).limit(amount)
+    var jobs = await _schema.find({ enddate: {$gte: new Date()} }).skip(amount * req.body.page).sort({postdate: -1, _id: -1}).limit(amount)
   }
   
   res.json({ jobs: jobs })
