1f96f9c762b190a3cec74259217134c1f9fb7c7b
Sokhavuth TIN <vuthdevelop@gmail.com>
2021-04-27 14:41:44 +0700

    Make it better

--- components/header.js
+++ components/header.js
@@ -31,7 +31,7 @@ export default function Header(){
         <div className={`${style.topnav} topnav`} id="topmenu">
           <Link href='/'><a onClick={() => loadingPage('/') } className={style.active}>Home</a></Link>
           <Link href='https://www.khmerweb.app/'><a>Learning</a></Link>
-          <a href="#contact">Contact</a>
+          <a href="https://www.khmerweb.app/search/label/contact">Contact</a>
           <a href="#about">About</a>
           <a className={style.icon} onClick={openMobileMenu}>
             <i className="fa fa-bars"></i>
